577abe44ea37e72b63b4649301e5638a2ffcdc5b
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-11 18:56:26 -0700

    First commit

---
+++ Sandbox-Xcode/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * File: main.cpp
+ * --------------
+ * This is an empty C++ source file.
+ */
+
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
