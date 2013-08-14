c875c75820fa06ad4a1c7f3924a4fb5e9e7648d3
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-13 19:16:45 -0700

    Learning GWindow library

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -1,35 +1,59 @@
 /*
  * File: main.cpp
  * --------------
- * This is an empty C++ source file.
+ * Playing with the GWindow library!
+ * Created: 2013-08-13
  */
 
-#include <iostream>
-#include "console.h"
-#include "random.h"
-#include "vector.h"
+#include "gwindow.h"
 
 using namespace std;
 
+/* Function prototypes */
 
-Vector<int> makeRandomVector(int sz)
-{
-	Vector<int> numbers;
-	for (int i = 0; i < sz; i++)
-		numbers.add(randomInteger(1, 100));
-	return numbers;
+void drawDiamond(GWindow &gw);
+void drawRectangleAndOval(GWindow &gw);
+
+/* Main program */
+
+int main() {
+    GWindow gw;
+    drawDiamond(gw);
+    drawRectangleAndOval(gw);
+    return 0;
 }
 
-void printVector(Vector<int> &v)
-{
-	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << " ";
-    cout << "" << endl;
+
+/*
+ * Function: drawDiamond
+ * Usage: drawDiamond(gw);
+ * -----------------------
+ * Draws a diamond connecting the midpoints of the window's edge.
+ */
+
+void drawDiamond(GWindow &gw) {
+    double width = gw.getWidth();
+    double height = gw.getHeight();
+    gw.drawLine(0, height / 2, width / 2, 0);
+    gw.drawLine(width / 2, 0, width, height / 2);
+    gw.drawLine(width, height / 2, width / 2, height);
+    gw.drawLine(width / 2, height, 0, height / 2);
 }
 
-int main()
-{
-	Vector<int> nums = makeRandomVector(10);
-	printVector(nums);
-	return 0;
+
+/*
+ * Function: drawRectangleAndOval
+ * Usage: drawRectangleAndOval(gw);
+ * --------------------------------
+ * Draws a blue rectangle and a gray oval inscribed in the diamond.
+ */
+
+void drawRectangleAndOval(GWindow &gw) {
+    double width = gw.getWidth();
+    double height = gw.getHeight();
+    gw.setColor("BLUE");
+    gw.fillRect(width / 4, height / 4, width / 2, height / 2);
+    gw.setColor("GRAY");
+    gw.fillOval(width / 4, height / 4, width / 2, height / 2);
 }
+
