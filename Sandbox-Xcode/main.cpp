3b844b875f4a82dc0db4e017c80d91901b89c6fc
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-15 22:57:52 -0500

    Learning Ch 3 strings

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -1,83 +1,29 @@
 /*
  * File: main.cpp
  * --------------
- * Playing with the GWindow library!
+ * Learning Strings, reading Ch 3.
  * Created: 2013-08-13
  */
 
-#include "gwindow.h"
-#include "gevents.h"
-#include "gobjects.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 /* Function prototypes */
 
-void drawDiamond(GWindow &gw);
-void drawRectangleAndOval(GWindow &gw);
 
 /* Main program */
 
 int main() {
-    GWindow gw;
-    drawDiamond(gw);
-    GLine *line;
-    /*while (true) {
-        GEvent e = waitForEvent(MOUSE_EVENT + KEY_EVENT);
-        switch (e.getEventClass()) {
-            case MOUSE_EVENT:
-                if (e.getEventType() == MOUSE_PRESSED) {
-                    line = new GLine(GMouseEvent(e).getX(), GMouseEvent(e).getY(),
-                                     GMouseEvent(e).getX(), GMouseEvent(e).getY());
-                    gw.add(line);
-                } else if (e.getEventType() == MOUSE_DRAGGED) {
-                    line->setEndPoint(GMouseEvent(e).getX(), GMouseEvent(e).getY());
-                }
-                
-                break;
-            case KEY_EVENT:
-                drawRectangleAndOval(gw);
-                break;
-        }
-        
-    }*/
-    GMouseEvent e = waitForEvent();
-    if (e.getEventType() == MOUSE_CLICKED) drawRectangleAndOval(gw);
-    
+    string name;
+    cout << "Enter your full name: ";
+    getline(cin, name);
+    cout << "Hello, " << name << "!" << endl;
     return 0;
 }
 
-
-/*
- * Function: drawDiamond
- * Usage: drawDiamond(gw);
- * -----------------------
- * Draws a diamond connecting the midpoints of the window's edge.
+/* This is interesting, because getline takes a stream. That stream
+ * could be the cin stream, or the fstream!
  */
-
-void drawDiamond(GWindow &gw) {
-    double width = gw.getWidth();
-    double height = gw.getHeight();
-    gw.drawLine(0, height / 2, width / 2, 0);
-    gw.drawLine(width / 2, 0, width, height / 2);
-    gw.drawLine(width, height / 2, width / 2, height);
-    gw.drawLine(width / 2, height, 0, height / 2);
-}
-
-
-/*
- * Function: drawRectangleAndOval
- * Usage: drawRectangleAndOval(gw);
- * --------------------------------
- * Draws a blue rectangle and a gray oval inscribed in the diamond.
- */
-
-void drawRectangleAndOval(GWindow &gw) {
-    double width = gw.getWidth();
-    double height = gw.getHeight();
-    gw.setColor("BLUE");
-    gw.fillRect(width / 4, height / 4, width / 2, height / 2);
-    gw.setColor("GRAY");
-    gw.fillOval(width / 4, height / 4, width / 2, height / 2);
-}
 
