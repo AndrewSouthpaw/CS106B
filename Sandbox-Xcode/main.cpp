3d5b5df582d67ed599792b6d3fac40d01dff577c
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-17 21:38:26 -0500

    Learning about file reading

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -7,24 +7,66 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "simpio.h"
 #include "console.h"
+#include "strlib.h"
 
 using namespace std;
 
+/* Constants */
+
+const string DEFAULT_FILE_NAME = "CollectiveNouns.txt";
+
 /* Function prototypes */
 
+string askUserForFile(ifstream &infile, string prompt = "");
 
 /* Main program */
 
 int main() {
     cout << "This program teaches you about collective nouns!" << endl;
-    cout << "Source: http://www.npwrc.usgs.gov/about/faqs/animals/names.htm" << endl;
-    cout << "What would you call a group of... ";
-    
+    cout << "Do you have a special file? (Y/N) ";
+    string ans = getLine();
+    ifstream infile;
+    if (ans.find('y') != string::npos || ans.find('Y') != string::npos) {
+        string filename = askUserForFile(infile, "Enter file name: ");
+        infile.open(filename.c_str());
+    } else {
+        cout << "Reading from default file..." << endl;
+        cout << "Source: http://www.npwrc.usgs.gov/about/faqs/animals/names.htm" << endl;
+        infile.open(DEFAULT_FILE_NAME.c_str());
+    }
+//    while (true) {
+//        cout << "What would you call a group of... (Press RETURN to quit) ";
+//        string entry = getLine();
+//        if (entry == "") break;
+//        string collective = toLowerCase(lookUpCollective(entry));
+//        cout << "A group of " << entry << " is " << collective << "." << endl;
+//    }
+
+    infile.close();
 }
 
-/* This is interesting, because getline takes a stream. That stream
- * could be the cin stream, or the fstream!
+
+/*
+ * Function: askUserForFile
+ * Usage: string file = askUserForFile(infile, "Enter name: ");
+ * ------------------------------------------------------------
+ * Asks a user for file. Tests whether the file is valid. If it is not,
+ * the user is informed and giving additional opportunities to enter a valid
+ * file name. The function returns the name of the file.
  */
+
+string askUserForFile(ifstream &infile, string prompt) {
+    if (prompt == "") prompt = "Enter file: ";
+    while (true) {
+        cout << prompt;
+        string filename = getLine();
+        infile.open(filename.c_str());
+        if (!infile.fail()) return filename;
+        infile.clear();
+        cout << "Invalid file. Please try again." << endl;
+    }
+}
 
