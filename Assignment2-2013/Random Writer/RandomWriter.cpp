008b8c32dcf268346beef0a5ef17a65110290820
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-26 03:13:49 -0500

    Assignment 2 - part 2 (random writer) begun

--- Assignment2-2013/Random Writer/RandomWriter.cpp
+++ Assignment2-2013/Random Writer/RandomWriter.cpp
@@ -1,17 +1,65 @@
 /*
  * File: RandomWriter.cpp
  * ----------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
- * This file is the starter project for the Random Writer problem.
- * [TODO: rewrite the documentation]
+ * Name: Andrew Smith
+ * Based off starter files in Stanford CS106B Summer 2013 class.
+ *
+ * This program randomly generates text based on a Markov model for character
+ * analysis.
  */
 
+#include <fstream>
 #include <iostream>
 #include "console.h"
+#include "simpio.h"
+#include "map.h"
+#include "vector.h"
 using namespace std;
 
+/* Constants */
+
+
+
+/* Function prototypes */
+
+string askUserForFile(ifstream &infile, string prompt);
+
+
+
+
+/* Main program */
+
 int main() {
-    // [TODO: fill in the code]
+	cout << "This program generates random text." << endl;
+	ifstream infile;
+	string file = askUserForFile(infile, "Enter sample text file: ");
+	
+	cout << "Analyzing file..." << endl;
+	Map<string, Vector<char> > frequencies;
+	
+	
     return 0;
+}
+
+
+
+/*
+ * Function: askUserForFile
+ * Usage: string file = askUserForFile(infile, "Enter name: ");
+ * ------------------------------------------------------------
+ * Asks a user for file. Tests whether the file is valid. If it is not,
+ * the user is informed and giving additional opportunities to enter a valid
+ * file name. The function returns the name of the file.
+ */
+
+string askUserForFile(ifstream &infile, string prompt) {
+    if (prompt == "") prompt = "Enter file: ";
+    while (true) {
+        cout << prompt;
+        string filename = getLine();
+        infile.open(filename.c_str());
+        if (!infile.fail()) return filename;
+        infile.clear();
+        cout << "Invalid file. Please try again." << endl;
+    }
 }
