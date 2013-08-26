c526b4f42b5efe459246a66ecc205dc52740fa94
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-26 02:23:18 -0500

    Assignment 2 - part 1 (word ladders) finished

--- Assignment2-2013/Word Ladder/WordLadder.cpp
+++ Assignment2-2013/Word Ladder/WordLadder.cpp
@@ -2,17 +2,202 @@
  * File: WordLadder.cpp
  * ----------------------
  * Name: Andrew Smith
- * This file is the starter project for the Word Ladder problem.
- * 
  *
+ * The Word Ladder searches for the shortest word ladders between two inputted words.
+ * A word ladder is a connection between two words by changing one letter
+ * at a time.
+ *
+ * Using starter files of Assignment 2 of Stanford CS106B Spring 2013 class.
  *
  */
 
 #include <iostream>
 #include "console.h"
+#include "lexicon.h"
+#include "vector.h"
+#include "queue.h"
+#include "simpio.h"
 using namespace std;
 
+
+/* Constants */
+
+const string LEXICON_FILE_NAME = "EnglishWords.dat";
+
+
+/* Function prototypes */
+
+Vector<string> findShortestLadder(Lexicon &words, string start, string end);
+Lexicon findWordsOffByOneLetter(Lexicon &words, string word);
+
+/* Main program */
+
 int main() {
-	// [TODO: fill in the code]
+	
+	Lexicon words(LEXICON_FILE_NAME);
+	
+	cout << "This program finds the shortest word ladder between "
+	<< "two words." << endl;
+	
+	while (true) {
+		
+		/* Input words */
+		cout << "Enter first word (or RETURN to quit): ";
+		string start = getLine();
+		if (start == "") break;
+		cout << "Enter second word: ";
+		string end = getLine();
+		
+		/* Check if word lengths match */
+		if (start.length() != end.length()) {
+			cout << "Word lengths do not match! Please try again." << endl;
+			
+			/* Search for word ladder */
+		} else {
+			cout << "Searching..." << endl;
+			
+			Vector<string> shortest = findShortestLadder(words, start, end);
+			
+			/* Check if a ladder was found */
+			if (shortest.size() == 0) {
+				cout << "No ladder found!" << endl;
+				
+				/* Print out the ladder */
+			} else {
+				string result;
+				for (int i = 0; i < shortest.size(); i++) {
+					if (i != 0) result += " -> ";
+					result += shortest[i];
+				}
+				cout << result << endl;
+			}
+		}
+	}
+	cout << "Goodbye!" << endl;
 	return 0;
 }
+
+
+
+/*
+ * Function: findShortestLadder
+ * Usage: Vector<string> ladder = findShortestLadder(words, start, end);
+ * ---------------------------------------------------------------------
+ * Returns the shortest word ladder between two words. Words must be of equal
+ * length. If no ladder is found, an empty vector is returned. A lexicon of
+ * words is used.
+ */
+
+
+
+/* Pseudocode
+ 
+ Create an empty queue.
+ Add the start word to the end of the queue. 
+ while (the queue is not empty) {
+	Dequeue the first ladder from the queue.
+	if (the final word in this ladder is the destination word){
+		Return this ladder as the solution.
+	}
+	for (each word in the lexicon of English words that differs by one letter){
+		if (that word has not already been used in a ladder) { 
+			Create a copy of the current ladder.
+			Add the new word to the end of the copy.
+			Add the new ladder to the end of the queue.
+			}
+		}
+	}
+ Report that no word ladder exists.
+ 
+ */
+
+
+Vector<string> findShortestLadder(Lexicon &words, string start, string end) {
+
+	/* Create an empty queue, ladder, and Lexicon of used words */
+	Queue<Vector<string> > ladders;
+	Vector<string> ladder;
+	Lexicon usedWords;
+	
+	/* Add the start word to the queue and the list of used words */
+	ladder.add(start);
+	ladders.enqueue(ladder);
+	usedWords.add(start);
+	
+	/* While the queue is not empty */
+	while (!ladders.isEmpty()) {
+		
+		ladder = ladders.dequeue();
+		string lastWord = ladder[ (ladder.size() - 1) ];
+		
+		/* Return the ladder if the final word is the end word */
+		if (lastWord == end) {
+			return ladder;
+		}
+		
+		/* Find words off by one letter from last word of ladder */
+		Lexicon wordsOffByOneLetter = findWordsOffByOneLetter(words, lastWord);
+		
+		/* For each word off by one letter, if it hasn't been used in a ladder,
+		 * create a copy of the current ladder and add the word to it, then
+		 * add it to the queue.
+		 */
+		for (string word : wordsOffByOneLetter) {
+			if (!usedWords.contains(word)) {
+				Vector<string> newLadder = ladder;
+				newLadder.add(word);
+				usedWords.add(word);
+				ladders.enqueue(newLadder);
+			}
+		}
+	}
+	
+	/* If the queue empties and a ladder has not successfully been returned,
+	 * no ladder exists; returns an empty vector.
+	 */
+	ladder.clear();
+	return ladder;
+}
+
+
+
+/*
+ * Function: findWordsOffByOneLetter
+ * Usage: Lexicon words = findWordsOffByOneLetter(words, lastWord);
+ * -----------------------------------------------------------------
+ * Returns a Lexicon of words that are off by one letter from the given word,
+ * based on a given Lexicon.
+ */
+ 
+Lexicon findWordsOffByOneLetter(Lexicon &words, string word) {
+
+	Lexicon result;
+	for (int i = 0; i < word.length(); i++) {
+		
+		/* Try replacing that letter with all 26 alphabet letters. If it forms
+		 * a new word, add it to the result.
+		 */
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			string str = word.substr(0, i) + ch + word.substr(i + 1, word.length());
+			if (words.contains(str)) {
+				result.add(str);
+			}
+		}
+	}
+	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -1,29 +1,7 @@
 /*
- * File: CheckoutLine.cpp
+ * File: main.cpp
  * --------------
- * Learning about models and simulations. Code lifted from "Programming
- * Abstractions in C++" by Eric Roberts, 2012, p. 222-4.
- *
- * Simulates a checkout line. Customers arrive at stand and get in line. Wait
- * until cashier is free, at which point they are served and take some period
- * of time. After service time completes, cashier is free to serve next customer
- * in line.
- *
- * In each unit of time, up to constant SIMULATION_TIME, each are performed:
- *
- * 1. Determine whether new customer arrives, with probability determined by
- *    constant ARRIVAL_PROBABILITY, using Poisson distribution.
- * 
- * 2. If cashier is busy, note cashier has spent another unit of time with that
- *    customer.
- *
- * 3. If cashier is free, serve next customer in line. Service time is random
- *    period between MIN_SERVICE_TIME and MAX_SERVICE_TIME.
- *
- * At end, report:
- * o The number of customers served
- * o The average time spent in line
- * o The average number of people in line
+ * General tinkering
  */
 
 #include <string>
@@ -48,95 +26,25 @@ using namespace std;
 
 /* Constants */
 
-const double ARRIVAL_PROBABILITY = 0.10;
-const int MIN_SERVICE_TIME = 5;
-const int MAX_SERVICE_TIME = 15;
-const int SIMULATION_TIME = 2000;
 
 
 /* Function prototypes */
 
-void runSimulation(int &nServed, int &totalWait, int &totalLength);
-void printReport(int nServed, int totalWait, int totalLength);
+
 
 /* Main program */
 
 int main() {
-	int nServed;
-	int totalWait;
-	int totalLength;
-	runSimulation(nServed, totalWait, totalLength);
-	printReport(nServed, totalWait, totalLength);
-	return 0;
+	Lexicon words("EnglishWords.dat");
+	test();
 }
 
 
-
-/*
- * Function: runSimulation
- * Usage: runSimulation(nServed, totalWait, totalLength);
- * -------------------------
- * Runs simulation.
- */
-
-void runSimulation(int &nServed, int &totalWait, int &totalLength) {
-	Queue<int> queue;
-	int timeRemaining = 0;
-	nServed = 0;
-	totalWait = 0;
-	totalLength = 0;
-	for (int t = 0; t < SIMULATION_TIME; t++) {
-		if (randomChance(ARRIVAL_PROBABILITY)) {
-			queue.enqueue(t);
-		}
-		if (timeRemaining > 0) {
-			timeRemaining--;
-		} else if (!queue.isEmpty()) {
-			totalWait += t - queue.dequeue();
-			nServed++;
-			timeRemaining = randomInteger(MIN_SERVICE_TIME, MAX_SERVICE_TIME);
-		}
-		totalLength += queue.size();
+void test() {
+	string testWord = "people";
+	if (words.contains(testWord)) {
+		cout << testWord << " is a word." << endl;
+	} else {
+		cout << testWord << " is not a word." << endl;
 	}
 }
-
-
-/*
- * Function: printReport
- * Usage: printReport(nServed, totalWait, totalLength);
- * ---------------------------------------------------
- * This function reports the results of the simulation.
- */
-
-void printReport(int nServed, int totalWait, int totalLength) {
-	cout << "Simulation results given the following constants:" << endl;
-	cout << fixed << setprecision(2);
-	cout << "SIMULATION_TIME: " << SIMULATION_TIME << endl;
-	cout << "ARRIVAL_PROBABILITY: " << ARRIVAL_PROBABILITY << endl;
-	cout << "MIN_SERVICE_TIME: " << MIN_SERVICE_TIME << endl;
-	cout << "MAX_SERVICE_TIME: " << MAX_SERVICE_TIME << endl;
-	cout << endl;
-	cout << "Customers served: " << nServed << endl;
-	cout << "Average waiting time: " << double(totalWait) / nServed << endl;
-	cout << "Average queue length: " << double(totalLength) / SIMULATION_TIME << endl;
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
