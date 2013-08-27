5551e8f29fa3ba10c023a42f083b1c6c054c819d
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-08-26 20:11:00 -0500

    Reading Ch 6 - designing classes
    Learning the Point definition!

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -34,37 +34,117 @@ using namespace std;
 
 /* Main program */
 
-int main() {
-	int stringToInteger(string str) {
-		istringstream stream(str);
-		int value;
-		stream >> value >> ws;
-		if (stream.fail() || !stream.eof()) {
-			error("stringToInteger: Illegal integer format");
-		}
-		return value;
-	}
-	
-	
-	string integerToString(int n) {
-		ostringstream stream;
-		stream << n;
-		return stream.str();
-	}
-	
-	int getInteger(string prompt) {
-		int value;
-		string line;
-		while (true) {
-			cout << prompt;
-			getline(cin, line);
-			istringstream stream(line);
-			stream >> value >> ws;
-			if (!stream.fail() && stream.eof()) break;
-			cout << "Illegal integer format. Try again." << endl;
-		}
-		return value;
-	}
-}
+//--- Point class interface ---
+/*
+ * File: point.h
+ * -------------
+ * This interface exports the Point class, which represents a point on
+ * a two-dimensional integer grid.
+ * (c) Eric Roberts, "Programming Abstractions in C++", 2013, p.269
+ */
+ 
+ #ifndef _point_h
+ #define _point_h
+ 
+ #include <string>
+ 
+ class point {
+ public:
+ 
+ /*
+ * Constructor: Point
+ * Usage: Point origin;
+ *        Point pt(xc, yc);
+ * ------------------------
+ * Creates a Point object. Default constructor sets coordinates to 0,
+ * the second form sets the coordinates to xc and yc.
+ */
+Point();
+Point (int xc, int yc);
+
+/*
+ * Methods: getX, getY
+ * Usage: int x = pt.getX();
+ *        int y = pt.getY();
+ * -------------------------
+ * These methods return the x and y coordinates of the point.
+ */
+
+int getX();
+int getY();
+
+/*
+ * Method: toString
+ * Usage: string str = pt.toString();
+ * ----------------------------------
+ * Returns a string expression of the Point in the form "(x,y)".
+ */
+
+std::string toString();
+
+private:
+int x;		/* The x-coordinate */
+int y;		/* The y-coordinate */
+};
+
+#endif
 
 
+
+
+//--- Point class implementation ---
+/*
+ * File: point.cpp
+ * ----------------
+ * This file implements the point.h interface.
+ * Taken from "Programming Abstractions in C++", Eric Roberts, 2013, p.270
+ */
+
+#include <string>
+#include "point.h"
+#include "strlib.h"
+using namespace std;
+
+/*
+ * Implementation notes: Constructors
+ * ----------------------------------
+ * The constructors initialize the instance variables x and y. In the
+ * second form of the constructor, the parameter names are xc and yc to
+ * avoid problem of shadowing the ivars.
+ */
+
+Point::Point() {
+	
+	Point::Point() {
+		x = 0;
+		y = 0;
+	}
+	Point::Point (int xc, int yc) {
+		x = xc;
+		y = yc;
+	}
+	
+	/*
+	 * Implementation notes: Getters
+	 * -----------------------------
+	 * The getters return the value of the corresponding instance variable.
+	 * No setters provided to ensure Point objects are immutable.
+	 */
+	
+	int Point::getX() {
+		return x;
+	}
+	int Point::getY() {
+		return y;
+	}
+	
+	/*
+	 * Implementation notes: toString
+	 * ------------------------------
+	 * The implementation of the toString uses the integerToString function
+	 * from the strlib.h interface.
+	 */
+	string Point::toString() {
+		return "(" + integerToString(x) + ","
+		+ integerToString(y) + ")";
+	}
