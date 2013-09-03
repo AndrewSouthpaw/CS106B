f549297efd167d706a3c97203909f541ae9d0ed3
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-02 20:02:43 -0700

    Learning pointers

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -24,25 +24,28 @@
 
 using namespace std;
 
-/* Constants */
 
 
 
-/* Function prototypes */
-
-
-
-
-
-bool dumbSolve(puzzleT puzzle, string lettersToAssign) {
-	if (lettersToAssign == "")
-		return puzzleSolved(puzzle);
+int main() {
+	int num;
+	int *p, *q;
 	
-	for (int digit = 0; digit <= 9; digit++) {
-		if(assignLetter(lettersToAssign[0], digit)) {
-			if (dumbSolve(puzzle, lettersToAssign.substr(1))) return true;
-			unassignLetter(lettersToAssign[0], digit);
-		}
-	}
-	return false;
+	p = new int;
+	*p = 10;
+	
+	q = new int;
+	*q = *p;	// dereference p and copy value in p address
+	
+	q = p;	// take address in p and assign to q; now they are aliases
+	for same location
+		
+	/* we do our own garbage collection */
+		delete p;
+	delete q;	// currently an error, because q is pointing to p
+	
+	q = NULL;
+	
+	/* the original value assigned to q is now orphaned, you can't get
+	 to it in any way */
 }
