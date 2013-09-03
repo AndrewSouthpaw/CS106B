161ffd467d7ae4a48dd5551398e369947f67a6d0
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-02 23:38:17 -0700

    Setting up Section 3 practice assignment

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -4,22 +4,10 @@
  * General tinkering
  */
 
-#include <string>
 #include <iostream>
-#include <cctype>
-#include <fstream>
-#include <iomanip>
 #include "console.h"
-#include "error.h"
-#include "simpio.h"
-#include "tokenscanner.h"
-#include "strlib.h"
-#include "map.h"
 #include "vector.h"
-#include "queue.h"
-#include "filelib.h"
-#include "lexicon.h"
-#include "random.h"
+#include "simpio.h"
 
 
 using namespace std;
---
+++ Section3-2013/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File: Problem1-WeightsandBalances.cpp
+ * -------------------------------------
+ * Simulates a simple two-pan balance and set of weights.  User enters the set of
+ * weights available.  User then can find out whether a certain weight can be
+ * measured using the set of weights.
+ */
+
+#include <iostream>
+#include "console.h"
+#include "simpio.h"
+#include "vector.h"
+
+using namespace std;
+
+int main() {
+	
+    return 0;
+}
