f93eb4498fadcf2efb01abeed0a1d2ac813ff033
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-04 21:48:38 -0700

    Section 3 Problem 1

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -16,24 +16,7 @@ using namespace std;
 
 
 int main() {
-	int num;
-	int *p, *q;
-	
-	p = new int;
-	*p = 10;
-	
-	q = new int;
-	*q = *p;	// dereference p and copy value in p address
-	
-	q = p;	// take address in p and assign to q; now they are aliases
-	for same location
-		
-	/* we do our own garbage collection */
-		delete p;
-	delete q;	// currently an error, because q is pointing to p
-	
-	q = NULL;
-	
-	/* the original value assigned to q is now orphaned, you can't get
-	 to it in any way */
+	Vector<int> v;
+	v += 1, 2, 3;
+	cout << "Sum is: " << sum(v) << endl;
 }
--- Section3-2013/main.cpp
+++ Section3-2013/main.cpp
@@ -17,6 +17,9 @@ using namespace std;
 /* Constants */
 const int SENTINEL = 0;
 
+/* Function prototypes */
+
+
 
 int main() {
 	
@@ -33,5 +36,81 @@ int main() {
 		}
 	}
 	
+	/* TEST FOR 0 WEIGHT CONDITION */
+	
+	/* Allow user to test if weights are measurable until SENTINEL entered */
+	cout << "Enter weights you want to test if they are measurable." << endl;
+	while (true) {
+		int wt = getInteger("?: ");
+		if (wt == SENTINEL) break;
+		if (wt < 0) {
+			cout << "Invalid weight. Please try again." << endl;
+		} else {
+			cout << "A weight of " << wt << ((isMeasureable(wt, weights)) ? " is " : " is not ");
+			cout << "measurable." << endl;
+		}
+	}
+	
     return 0;
 }
+
+/*
+ * Function: isMeasureable
+ * Usage: isMeasureable(wt, weights);
+ * ----------------------------------
+ * Recursively tests if a weight is measureable using a two-pan balance given
+ * a set of weights.
+ */
+
+bool isMeasureable(int wt, Vector<int> weights) {
+	
+	/* Suppose the weight to be measured is always on the left pan.
+	 * Each weight in the set can be included on the left pan, right pan,
+	 * or neither.
+	 * Base case reached when the weight on the left equals the set of weights
+	 * remaining, or when there are no weights left to be considered.
+	 * But how to store where the weight is located?!
+	 * Do you add it to the wt and remove it from the weights set? Yes.
+	 * So there are two branches? One where the setWt is added to wt and removed
+	 * from set, and one where it's just removed?
+	 * Almost, but there's an issue with ordering with that strategy. If it is
+	 * either added to wt or removed, there's no time when the 1st and last
+	 * setWt elements would be tested together.
+	 * So that would be a permutation issue.
+	 * I think that would do it, but it might get messy.
+	 */
+	
+	/* Base case: weight to be measured equals remaining weights in set */
+	int sum = 0;
+	for (int n : weights) {
+		sum += n;
+	}
+	
+	if (wt == sum) return true;
+	if (sum == 0) {
+		return false;
+	} else {
+		for (int i = 0; i < weights.size(); i++) {
+			int next = weights[i];
+			isMeasureable(wt + next)
+		}
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
