24913f086e94db06409bb063a6044dca85450170
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-07 21:47:20 -0700

    Section 3 - Problem 4 - pyramid counting - finished
    A recursive problem that I completed successfully! Win.

--- Section3-2013/main.cpp
+++ Section3-2013/main.cpp
@@ -1,8 +1,10 @@
 /*
- * File: MultiWordAnagrams.cpp
+ * File: Problem4_2009_Cannonballs.cpp
  * -------------------------------------
- * Generates multi-word anagrams from an input by user. If no anagram exists,
- * the program states as such.
+ * Cannonballs are stacked in a pyramid shade: one cannonball sits on top of
+ * a square of 4, on top of a square of 9, and so on.
+ * Takes the number of layers in the pyramid from a user and reports the
+ * cannonballs in the pyramid.
  */
 
 #include <iostream>
@@ -16,114 +18,42 @@ using namespace std;
 
 
 /* Constants */
-const int MIN_WORD_SIZE = 3;
-const string LEXICON_FILE_NAME = "EnglishWords.dat";
 
 
 /* Function prototypes */
-bool findAnagram(string letters, Lexicon &english, Vector<string> &words);
-string scrubInput(string line);
-bool findAnagramWithPrefix(string prefix, string rest,
-						   Lexicon &english, Vector<string> &words);
+int cannonball(int height);
 
 /* Main program */
 
 int main() {
 
-	Lexicon english(LEXICON_FILE_NAME);
-	
-	cout << "This program produces multi-word anagrams." << endl;
-	cout << "Minimum word length: " << MIN_WORD_SIZE << endl;
-	
 	while (true) {
-		string line = getLine("Enter a line: (ENTER to exit) ");
-		if (line == "") break;
-		Vector<string> words;
-		if (findAnagram(scrubInput(line), english, words)) {
-			cout << "An anagram exists." << endl;
-			for (string word : words) {
-				cout << word << " ";
-			}
-			cout << endl;
-		} else {
-			cout << "Sorry, an anagram does not exist." << endl;
-		}
+		int height = getInteger("Number of layers in the cannonball pyramid: ");
+		if (height == 0) break;
+		int balls = cannonball(height);
+		cout << "Number of balls: " << balls << endl;
 	}
-	
 	return 0;
 }
 
 
 /*
- * Function: scrubInput
- * Usage: string clean = scrubInput(line);
- * ---------------------------------------
- * Scrubs the line of spaces and punctuation.
+ * Function: cannonball
+ * Usage: numBalls = cannonball(height);
+ * -------------------------------------
+ * Returns the number of balls in a pyramid based on the number of layers.
  */
 
-string scrubInput(string line) {
-	string result;
-	for (char ch : line) {
-		if (isalpha(ch)) {
-			result += tolower(ch);
-		}
+int cannonball(int height) {
+	if (height < 1) {
+		return 0;
 	}
-	return result;
-}
-
-
-/*
- * Function: findAnagram
- * Usage: findAnagram(letters, english, words);
- * --------------------------------------------
- * Checks for existence of multi-word anagram composed of the letters, using
- * a lexicon for reference. Passes back a Vector<string> of the words.
- * Returns true if an anagram is found, false otherwise.
- */
-
-/*
- * Recursive strategy
- * ------------------
- 
- This is a permutation problem. Pick a letter, pick another letter, etc.,
- until you run out of letters. Check if it is an anagram. If it's not,
- back up one step and try another letter.
- 
- */
-
-bool findAnagram(string letters, Lexicon &english, Vector<string> &words) {
-	return findAnagramWithPrefix("", letters, english, words);
-}
-
-
-/*
- * Function: findAnagramWithPrefix
- * Usage: findAnagramWithPrefix("", letters, english, words);
- * ----------------------------------------------------------
- * Finds a multiword anagram for a set of letter, where the current word
- * must begin with the specified prefix.
- */
-
-bool findAnagramWithPrefix(string prefix, string rest,
-						   Lexicon &english, Vector<string> &words) {
-	if (!english.containsPrefix(prefix)) return false;
-	if (english.contains(prefix) && prefix.length() >= MIN_WORD_SIZE) {
-		if (rest == "" || findAnagram(rest, english, words)) {
-			words.add(prefix);
-			return true;
-		}
+	if (height == 1) {
+		return 1;
 	}
-	for (int i = 0; i < rest.size(); i++) {
-		string otherLetters = rest.substr(0, i) + rest.substr(i+1);
-		if (findAnagramWithPrefix(prefix + rest[i], otherLetters, english, words)) {
-			return true;
-		}
-	}
-	return false;
+	return height * height + cannonball(height - 1);
+	
 }
-
-
-
 
 
 
