96476e4897544e24d6696f02c0afd66e573b3999
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-09 20:12:04 -0700

    Section 3 - Problem 5 - reverse string recursively

--- Section3-2013/main.cpp
+++ Section3-2013/main.cpp
@@ -1,10 +1,7 @@
 /*
- * File: Problem4_2009_Cannonballs.cpp
+ * File: Problem5_2009_ReverseString.cpp
  * -------------------------------------
- * Cannonballs are stacked in a pyramid shade: one cannonball sits on top of
- * a square of 4, on top of a square of 9, and so on.
- * Takes the number of layers in the pyramid from a user and reports the
- * cannonballs in the pyramid.
+ * Recursively reverses a string.
  */
 
 #include <iostream>
@@ -21,7 +18,7 @@ using namespace std;
 
 
 /* Function prototypes */
-int cannonball(int height);
+string reverseString(string str);
 
 
 /* Main program */
@@ -29,31 +26,25 @@ int cannonball(int height);
 int main() {
 
 	while (true) {
-		int height = getInteger("Number of layers in the cannonball pyramid: ");
-		if (height == 0) break;
-		int balls = cannonball(height);
-		cout << "Number of balls: " << balls << endl;
+		string str = getLine("?: ");
+		if (str == "") break;
+		string rev = reverseString(str);
+		cout << rev << endl;
 	}
 	return 0;
 }
 
 
 /*
- * Function: cannonball
- * Usage: numBalls = cannonball(height);
- * -------------------------------------
- * Returns the number of balls in a pyramid based on the number of layers.
+ * Function: reverseString
+ * Usage: string rev = reverseString(str);
+ * ---------------------------------------
+ * Recursively reverses a string.
  */
 
-int cannonball(int height) {
-	if (height < 1) {
-		return 0;
-	}
-	if (height == 1) {
-		return 1;
-	}
-	return height * height + cannonball(height - 1);
-	
+string reverseString(string str) {
+	if (str == "") return "";
+	return reverseString(str.substr(1)) + str[0];
 }
 
 
