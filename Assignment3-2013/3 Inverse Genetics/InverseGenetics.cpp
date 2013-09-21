49cd3bba2ae24d6ee0ae44f201d051c790e118cf
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-20 22:43:52 -0700

    Assignment 3 - Problem 3 completed

--- Assignment3-2013/3 Inverse Genetics/InverseGenetics.cpp
+++ Assignment3-2013/3 Inverse Genetics/InverseGenetics.cpp
@@ -1,11 +1,10 @@
 /*
  * File: InverseGenetics.cpp
  * --------------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
- * This file is the starter project for the Inverse Genetics problem
- * on Assignment #3.
- * [TODO: extend the documentation]
+ * Name: Andrew Smith
+ *
+ * Lists the possible RNA strands for a protein string provided by user.
+ * Assumes all protein strings are valid.
  */
 
 #include <iostream>
@@ -14,34 +13,39 @@
 #include "set.h"
 #include "map.h"
 #include "console.h"
+#include "simpio.h"
 using namespace std;
 
-/* Function: listAllRNAStrandsFor(string protein,
- *                                Map<char, Set<string> >& codons);
- * Usage: listAllRNAStrandsFor("PARTY", codons);
- * ==================================================================
- * Given a protein and a map from amino acid codes to the codons for
- * that code, lists all possible RNA strands that could generate
- * that protein
- */
+
+/* Function prototypes */
+
 void listAllRNAStrandsFor(string protein, Map<char, Set<string> >& codons);
+Map<char, Set<string> > loadCodonMap();
+void recurAllRNAStrandsFor(string soFar, string protein, Map<char,
+                           Set<string> > &codons);
+
+/* Main program */
+
+int main() {
+    /* Load the codon map. */
+    Map<char, Set<string> > codons = loadCodonMap();
+
+    cout << "This program lists the RNA strands possible for a protein string." << endl;
+    while (true) {
+        string protein = getLine("Enter a protein string: ");
+        if (protein == "") break;
+        listAllRNAStrandsFor(protein, codons);
+    }
+    
+    return 0;
+}
 
 /* Function: loadCodonMap();
  * Usage: Map<char, Lexicon> codonMap = loadCodonMap();
  * ==================================================================
  * Loads the codon mapping table from a file.
  */
-Map<char, Set<string> > loadCodonMap();
 
-int main() {
-    /* Load the codon map. */
-    Map<char, Set<string> > codons = loadCodonMap();
-
-    /* [TODO: Implement this!] */
-    return 0;
-}
-
-/* You do not need to change this function. */
 Map<char, Set<string> > loadCodonMap() {
     ifstream input("codons.txt");
     Map<char, Set<string> > result;
@@ -59,3 +63,51 @@ Map<char, Set<string> > loadCodonMap() {
     
     return result;
 }
+
+/*
+ * Function: listAllRNAStrandsFor
+ * Usage: listAllRNAStrandsFor("PARTY", codons);
+ * ---------------------------------------------
+ * Given a protein and a map from amino acid codes to the codons for
+ * that code, lists all possible RNA strands that could generate
+ * that protein. Assumes all protein letters are valid.
+ */
+
+void listAllRNAStrandsFor(string protein, Map<char, Set<string> > &codons) {
+    recurAllRNAStrandsFor("", protein, codons);
+}
+
+/*
+ * Function: recurAllRNAStrandsFor
+ * Usage: recurAllRNAStrandsFor("", protein, codons);
+ * --------------------------------------------------
+ * Recursively lists the RNA strands for a protein.
+ */
+    
+void recurAllRNAStrandsFor(string soFar, string protein, Map<char,
+                           Set<string> > &codons) {
+    
+    /* Base case: empty protein string */
+    if (protein.length() == 0) {
+        cout << soFar << endl;
+        return;
+    }
+    
+    /* Recur step:
+     * 1. Take the next letter in the protein.
+     * 2. Recur for each RNA sequence
+     */
+    Set<string> strands = codons.get(protein[0]);
+    for (string strand : strands) {
+        recurAllRNAStrandsFor(soFar + strand, protein.substr(1), codons);
+    }
+}
+
+
+
+
+
+
+
+
+
