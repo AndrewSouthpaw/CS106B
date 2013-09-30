a055c76def04dd2899a70a8e8d063b2d99bff6fd
Andrew Smith <andrewsmith@alumni.stanford.edu>
2013-09-29 22:09:55 -0700

    Learning quicksort, templates

--- Sandbox-Xcode/main.cpp
+++ Sandbox-Xcode/main.cpp
@@ -11,53 +11,51 @@
 #include "simpio.h"
 #include "vector.h"
 #include "queue.h"
+#include "lexicon.h"
 using namespace std;
 
-/* Constants */
 
 
-/* Function prototypes */
+/* Quicksort! */
 
-// This one is for Queues... write one for Vectors.
-void merge(Queue<int> &one, Queue<int> &two, Queue<int> &result) {
-	while (!one.isEmpty() && !two.isEmpty()) {
-		if (one.peek() < two.peek()) {
-			result.enqueue(one.dequeue());
-		} else {
-			result.enqueue(two.dequeue());
-		}
-	}
+int partition(Vector<int> &arr, int start, int stop) {
+	int lh = start + 1, rh = stop, pivot;
 	
-	while (!one.isEmpty()) {
-		result.enqueue(one.dequeue());
+	pivot = arr[start];
+	while (true) {
+		while (lh < rh && arr[rh] >= pivot) rh--;
+		// find something that is not greater than pivot
+		while (lh < rh && arr[lh] < pivot) lh++;
+		// find something that doesn't belong on lh
+		if (lh == rh) break;
+		swap(arr[lh], arr[rh]);
+		// exchange those two! problems go away.
+		// invariant: arr[start+1..lh] < pivot && arr[rh..stop] >= pivot
 	}
-	while (!two.isEmpty()) {
-		result.enqueue(two.dequeue());
+	if (arr[lh] >= pivot) return start;
+	swap(arr[start], arr[lh]);
+	// swap pivot into place where lh and rh markers cross
+	return lh;
+}
+
+void quicksort(Vector<int> &arr, int start, int stop) {
+	if (stop > start) {
+		int pivot = partition(arr, start, stop);
+		quicksort(arr, start, pivot - 1);
+		quicksort(arr, pivot + 1, stop);
 	}
 }
 
-void merge(Vector<int> &one, Vector<int> &two, Vector<int> &result) {
-	int p, p1, p2;
-	p = p1 = p2 = 0;
-	while (p1 < one.size() && p2 < two.size()) {
-		if (one[p1] < two[p2])
-			result[p++] = one[p1++];
-		else
-			result[p++] = two[p2++];
-	}
-	while (p1 < one.size()) result[p++] = one[p1++];
-	while (p2 < two.size()) result[p++] = two[p2++];
-}
 
-void mergeSort(Vector<int> &v) {
-	if (v.size() > 1) {
-		int n1 = v.size()/2;
-		int n2 = v.size() - n1;
-		Vector<int> left = copy(v, 0, n1);		// O(N)
-		Vector<int> right = copy(v, n1, n2);	// O(N)
-		mergeSort(left);
-		mergeSort(right);
-		merge(v, left, right);					// O(N)
-	}
-}
+
+
+
+
+
+
+
+
+
+
+
 
