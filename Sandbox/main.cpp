eb23f83f166bb5107da1e8cff5bb299a6daf9b65
Andrew Smith <andrewsmith@alumni.stanford.edu>
2014-05-23 13:55:30 +0200

    testing quick sort knowledge

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -10,6 +10,7 @@
 #include "vector.h"
 
 
+
 using namespace std;
 
 /* Prototypes */
@@ -54,17 +55,91 @@ void copyStream(istream& is, ostream& os) {
  
  */
 
+int choosePivot(Vector<int> &v, int low, int high) {
+	return low;
+}
+
+int partition(Vector<int> &v, int pivotIndex, int low, int high) {
+	
+	/* Record pivot value, move pivot to front of array */
+	int pivot = v[pivotIndex];
+	swap(v[low], v[pivotIndex]);
+	
+	int left = low + 1;
+	int right = high;
+	
+	/* Move left and right markers toward the middle. Swap when a high value
+	 * is found on the left and a low value on the right.
+	 */
+	while (left < right) {
+		while (left < right && v[right] >= pivot) --right;
+		while (left < right && v[left] < pivot) ++left;
+		swap(v[left], v[right]);
+	}
+	/* If pivot is less than meeting point of pointers, pivot already in place */
+	if (pivot < v[left]) return low;
+	
+	/* Move pivot "in place" to the point where the pointers meet if it
+	 * is less than that element.
+	 */
+	else {
+		swap(v[low], v[left]);
+		return left;
+	}
+	
+}
+
+void quicksort(Vector<int> &v, int low, int high) {
+	if (low >= high) return;
+	
+	int pivotIndex = choosePivot(v, low, high);
+	int partitionIndex = partition(v, pivotIndex, low, high);
+	quicksort(v, low, partitionIndex - 1);
+	quicksort(v, partitionIndex + 1, high);
+	
+}
+
+//int partition(Vector<int>& v, int low, int high) {
+//	int pivot = v[low];
+//	int left = low + 1, right = high;
+//	
+//	while (left < right) {
+//		while (left < right && v[right] >= pivot) --right;
+//		while (left < right && v[left] < pivot) ++left;
+//		
+//		if (left < right) swap(v[left], v[right]);
+//	}
+//	if (pivot < v[right]) return low;
+//	swap(v[low], v[right]);
+//	return right;
+//}
+//
+//void quicksort(Vector<int> &v, int low, int high) {
+//	if (low >= high) return;
+//	
+//	int partitionPoint = partition(v, low, high);
+//	quicksort(v, low, partitionPoint - 1);
+//	quicksort(v, partitionPoint + 1, high);
+//}
+
+void readVector(Vector<int> &v, int l, int r) {
+	for (int i = l; i < r; i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
 
 int main() {
 	
-	ifstream infile;
-	promptUserForFile(infile, "Input file: ");
-	//	char ch;
-	//	while (infile.get(ch)) {
-	//		cout.put(ch);
-	//	}
-	copyStream(infile, cout);
-	infile.close();
+	Vector<int> v;
+	v += 3, 2, 4, 8, 1, 5, 7, 6;
+	
+	quicksort(v, 0, v.size() - 1);
+	readVector(v, 0, v.size());
+	
+	
+
+	
 	
 	
 	return 0;
